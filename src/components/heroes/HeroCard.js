3ae1f84fd24b647ff9ca1276a9022ee98df3d5df
William Park <l.jimenez.leon97@gmail.com>
2021-07-07 19:02:15 -0500

    veamo

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
-// import { heroImages } from '../../helpers/heroImages';
+import { heroImages } from '../../helpers/heroImages';
 import "./heroCard.css";
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
     return (
         <Link to={`./hero/${id}`} className="my-card">
             <img 
-                src={ `./docs/assets/heroes/${ id }.jpg` } //build de produccion
+                // src={ `./docs/assets/heroes/${ id }.jpg` } //build de produccion
                 // src={ `./assets/heroes/${ id }.jpg` } 
-                // src={ heroImages(`./${ id }.jpg`).default }
+                src={ heroImages(`./${ id }.jpg`).default }
                 className="img img-responsive" 
                 alt={ superhero } 
             />
